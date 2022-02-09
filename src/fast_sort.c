8214c0b950856bd21d23e6dd0875d07ffd511f99
unnamed <arrynpal@ox-i3.msk.21-school.ru>
2022-02-09 17:10:19 +0300

    fast_sort

--- src/fast_sort.c
+++ src/fast_sort.c
@@ -27,7 +27,7 @@ int input(int *a, int *n) {
     for (int *p = a; p - a < *n; p++) {
         if (!scanf("%d", p)) return 0;
         if ((int *) p  != p) return 0;
-        if (getchar() == 10) return 0;
+        if (p - a < *n - 1 && getchar() == 10) return 0;
     }
     if (getchar() != 10) return 0;
     return 1;
