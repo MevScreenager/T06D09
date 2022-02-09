febb8598d40f2ee4bae6871ab9b2198b1d07a8d2
unnamed <arrynpal@ox-i3.msk.21-school.ru>
2022-02-09 18:13:56 +0300

    cycle_shift

--- src/cycle_shift.c
+++ src/cycle_shift.c
@@ -19,11 +19,12 @@ int main() {
 int input(int *a, int *n, int *m) {
     if (!scanf("%d", n)) return 0;
     if ((int *) n  != n) return 0;
+    if (*n < 0) return 0;
 
     for (int *p = a; p - a < *n; p++) {
         if (!scanf("%d", p)) return 0;
         if ((int *) p  != p) return 0;
-        if (getchar() == 10) return 0;
+        if (p - a < *n - 1 && getchar() == 10) return 0;
     }
     if (!scanf("%d", m)) return 0;
     if ((int *) m  != m) return 0;
@@ -33,12 +34,21 @@ int input(int *a, int *n, int *m) {
 
 void move(int *a, int *n, int *m) {
     int arry[NMAX];
-    for (int *p = m; p - a < *n; p++)
-        arry[p - m] = *p;
-    for (int *p = a; p - a < *m; p++)
-        arry[n - m + (p - a)] = *p;
-    for (int *p = a; p - a < *n; p++)
-        *p = arry[p - a];
+    if (*m > 0) {
+        for (int *p = &a[*m]; p - a < *n; p++)
+            arry[p - &a[*m]] = *p;
+        for (int *p = a; p - a < *m; p++)
+            arry[(*n - *m) + (p - a)] = *p;
+        for (int *p = a; p - a < *n; p++)
+            *p = arry[p - a];
+    } if (*m < 0) {
+        for (int *p = &a[*n - (-1 * *m)]; p - a < *n; p++)
+            arry[p - &a[*n - (-1 * *m)]] = *p;
+        for (int *p = a; p - a < *n - (-1 * *m); p++)
+            arry[(-1 * *m) + (p - a)] = *p;
+        for (int *p = a; p - a < *n; p++)
+            *p = arry[p - a];
+    }
 }
 
 void output(int *a, int *n) {
